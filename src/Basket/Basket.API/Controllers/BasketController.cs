ad728bb4fe13c2a5d3ff8c7f9795e10ecf559d66
Yavuz SAV <yavuzsav07@gmail.com>
2020-08-08 15:37:25 +0300

    Created basket api

---
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+    }
+}
