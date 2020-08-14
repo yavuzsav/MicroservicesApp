1ecafe21d854ae527d91398f8fe6299466fd1792
Yavuz SAV <yavuzsav07@gmail.com>
2020-08-14 13:41:12 +0300

    Created IOrderRepository interface

---
+++ src/Ordering/Ordering.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Base;
+
+namespace Ordering.Core.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserNameAsync(string userName);
+    }
+}
