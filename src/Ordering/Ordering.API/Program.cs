6b23f27962ce990e3207fda4dbfae40c7fe6f427
Yavuz SAV <yavuzsav07@gmail.com>
2020-08-15 02:08:01 +0300

    Seed data

--- src/Ordering/Ordering.API/Program.cs
+++ src/Ordering/Ordering.API/Program.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.API
 {
@@ -13,7 +11,29 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateAndSeedDatabase(host);
+            host.Run();
+        }
+
+        private static void CreateAndSeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var orderContext = services.GetRequiredService<OrderContext>();
+                    OrderContextSeed.SeedAsync(orderContext, loggerFactory, 3).Wait();
+                }
+                catch (Exception e)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(e.Message);
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int retry = 0)
+        {
+            int retryForAvailability = 1;
+            try
+            {
+                await orderContext.Database.MigrateAsync();
+
+                if (!orderContext.Orders.Any())
+                {
+                    await orderContext.Orders.AddRangeAsync(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                if (retryForAvailability < retry)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(e.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order
+                {
+                    UserName = "swn", FirstName = "Bob", LastName = "Bobby", EmailAddress = "test@test.com",
+                    AddressLine = "London", Country = "England"
+                }
+            };
+        }
+    }
+}
